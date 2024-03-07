fc8415dbc12eb7ae532e0d03174ee3df41159e41
farheen-omniful <farheen.ali@omniful.com>
2024-03-07 21:29:48 +0530

    Day-2-practical

---
+++ JavaDataType_Day2/src/main/java/org/example/DayTwo.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class DayTwo {
+    public int a=8798;
+    public float b =1.0F;
+    public String str ="farheen";
+
+    public void add(){
+        System.out.println(a+b);
+    }
+
+    public String sub(){
+
+        System.out.println(a-b);
+        return str;
+    }
+}
---
+++ JavaDataType_Day2/src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        System.out.println("Hellofgdfkkgjsdlkfj world!");
+//        //Data type of java
+//        int a = 543;
+//        System.out.println(a+1);
+//
+//        String str ="farheen";
+//        System.out.println(str+"ali");
+//        System.out.println(str);
+//
+//        boolean status = false;
+
+        DayTwo dayTwo = new DayTwo();
+         dayTwo.add();
+         String ans = dayTwo.sub();
+        System.out.println(ans);
+
+    }
+}
